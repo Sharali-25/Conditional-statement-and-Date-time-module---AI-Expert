c7ad2598ec9827fd771c2469646bd2f2afe11a34
Sharali-25 <Sharaliarkansas@gmail.com>
2026-06-13 10:15:38 -0500

    Add files via upload

---
+++ ACP.py
@@ -0,0 +1,11 @@
+name = input("What's ur name ? ")
+print("Hi",name,"Welcome to the mood advisior")
+mood= input("How are you feeling today ? " ).lower()
+if mood == "happy":
+    print("Glad to hear! ")
+elif mood == "sad":
+    print("Im so sorry to hear that! I hope you are doing okay!")
+elif mood =="angry":
+    print("Take a deep breath, and rethink what you just did/rethink ur life chocies AGAIN!")
+else:
+    print("Sorry, I didnt understand that mood")
